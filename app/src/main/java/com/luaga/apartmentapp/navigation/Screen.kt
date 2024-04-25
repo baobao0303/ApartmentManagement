bd1ea3505b0ab033ca6f072ce598eeaf8aff5815
baobao0303 <baobao22.work@gmail.com>
2024-04-26 06:49:30 +0700

    delete v1.0

--- app/src/main/java/com/luaga/apartmentapp/navigation/Screen.kt
+++
@@ -1,8 +0,0 @@
-package com.luaga.apartmentapp.navigation
-
-sealed class  Screen (val route : String){
-    object HomeScreen: Screen(route = "home_screen")
-    object AddScreen: Screen(route = "add_screen")
-    object UserScreen: Screen(route = "user_screen")
-    object LoginScreen: Screen(route = "login_screen")
-}
