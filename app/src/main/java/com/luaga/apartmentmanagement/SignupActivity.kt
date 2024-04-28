a64d0791b68308127580193b1d2fc73e227d6338
baobao0303 <baobao22.work@gmail.com>
2024-04-29 02:26:12 +0700

    fix

--- app/src/main/java/com/luaga/apartmentmanagement/SignupActivity.kt
+++ app/src/main/java/com/luaga/apartmentmanagement/SignupActivity.kt
@@ -2,6 +2,7 @@ package com.luaga.apartmentmanagement
 
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.luaga.apartmentmanagement.databinding.ActivitySignupBinding
 
@@ -18,12 +19,45 @@ class SignupActivity : AppCompatActivity() {
 
         buttonBack = signupBinding.buttonBack
 
-        signupBinding.buttonSignup.setOnClickListener {}
+        signupBinding.buttonSignup.setOnClickListener {
+            val email = signupBinding.editTextSignUpEmail.text.toString()
+            val password = signupBinding.editTextSignUpPassword.text.toString()
+            val confirmPassword = signupBinding.editTextSignUpConfirmPassword.text.toString()
+
+            if (!email.contains("@")) {
+                showInvalidEmailDialog()
+            } else if(password != confirmPassword){
+                showPasswordMismatchDialog()
+            } else {
+                // Email format and passwords match, continue with sign-up process
+            }
+        }
 
         buttonBack!!.setOnClickListener {
-            // Xử lý sự kiện khi button được nhấn
-            // Ví dụ: Quay về trang trước đó
             onBackPressed()
         }
+    }
+    private fun showInvalidEmailDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Invalid Email")
+        builder.setMessage("Please enter a valid email address.")
+        builder.setPositiveButton("OK") { dialog, which ->
+            // Do nothing, just dismiss the dialog
+            dialog.dismiss()
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showPasswordMismatchDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Password Mismatch")
+        builder.setMessage("The passwords entered do not match. Please make sure your passwords match.")
+        builder.setPositiveButton("OK") { dialog, which ->
+            // Do nothing, just dismiss the dialog
+            dialog.dismiss()
+        }
+        val dialog = builder.create()
+        dialog.show()
     }
 }
