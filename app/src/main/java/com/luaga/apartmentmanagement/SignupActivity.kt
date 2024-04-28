6a344c8c6ed6180f1056441b5fb44bdd3e173d83
baobao0303 <baobao22.work@gmail.com>
2024-04-29 03:41:12 +0700

    creating an Account with email and password

--- app/src/main/java/com/luaga/apartmentmanagement/SignupActivity.kt
+++ app/src/main/java/com/luaga/apartmentmanagement/SignupActivity.kt
@@ -1,16 +1,19 @@
 package com.luaga.apartmentmanagement
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.luaga.apartmentmanagement.databinding.ActivitySignupBinding
 
 
 class SignupActivity : AppCompatActivity() {
     lateinit var signupBinding: ActivitySignupBinding
     var buttonBack: Button? = null
-
+    val auth : FirebaseAuth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         signupBinding = ActivitySignupBinding.inflate(layoutInflater)
@@ -18,18 +21,20 @@ class SignupActivity : AppCompatActivity() {
         setContentView(view)
 
         buttonBack = signupBinding.buttonBack
-
         signupBinding.buttonSignup.setOnClickListener {
             val email = signupBinding.editTextSignUpEmail.text.toString()
             val password = signupBinding.editTextSignUpPassword.text.toString()
             val confirmPassword = signupBinding.editTextSignUpConfirmPassword.text.toString()
 
-            if (!email.contains("@")) {
+            if (!isValidEmail(email)) {
                 showInvalidEmailDialog()
+            } else if(!isValidPassword(password)) {
+                showInvalidPasswordDialog()
             } else if(password != confirmPassword){
                 showPasswordMismatchDialog()
             } else {
                 // Email format and passwords match, continue with sign-up process
+                signupWithFirebase(email,password)
             }
         }
 
@@ -37,12 +42,45 @@ class SignupActivity : AppCompatActivity() {
             onBackPressed()
         }
     }
+    private fun isValidEmail(email: String): Boolean {
+        return email.contains("@") && email.count { it == '@' } == 1
+    }
+    private fun isValidPassword(password: String): Boolean {
+        return password.length >= 7
+    }
     private fun showInvalidEmailDialog() {
         val builder = AlertDialog.Builder(this)
-        builder.setTitle("Invalid Email")
-        builder.setMessage("Please enter a valid email address.")
+        builder.setTitle("Email Không Hợp Lệ")
+        builder.setMessage("Vui lòng nhập địa chỉ email hợp lệ chỉ chứa một ký tự '@'.")
         builder.setPositiveButton("OK") { dialog, which ->
-            // Do nothing, just dismiss the dialog
+            dialog.dismiss()
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+    fun signupWithFirebase(
+        email: String,
+        password: String
+    ){
+        signupBinding.progressBarSignup.visibility = View.VISIBLE
+        signupBinding.buttonSignup.isClickable = false
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
+            task -> if (task.isSuccessful){
+                        Toast.makeText(applicationContext,"Your account has been created", Toast.LENGTH_LONG).show()
+                        finish()
+                        signupBinding.progressBarSignup.visibility = View.INVISIBLE
+                        signupBinding.buttonSignup.isClickable= true
+                    }else {
+                        Toast.makeText(applicationContext,task.exception?.localizedMessage, Toast.LENGTH_SHORT).show()
+                    }
+        }
+    }
+
+    private fun showInvalidPasswordDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Mật Khẩu Không Hợp Lệ")
+        builder.setMessage("Vui lòng nhập mật khẩu có ít nhất 7 ký tự.")
+        builder.setPositiveButton("OK") { dialog, which ->
             dialog.dismiss()
         }
         val dialog = builder.create()
@@ -51,13 +89,13 @@ class SignupActivity : AppCompatActivity() {
 
     private fun showPasswordMismatchDialog() {
         val builder = AlertDialog.Builder(this)
-        builder.setTitle("Password Mismatch")
-        builder.setMessage("The passwords entered do not match. Please make sure your passwords match.")
+        builder.setTitle("Mật Khẩu Không Trùng Khớp")
+        builder.setMessage("Mật khẩu nhập lại không khớp với mật khẩu đã nhập. Vui lòng kiểm tra lại.")
         builder.setPositiveButton("OK") { dialog, which ->
-            // Do nothing, just dismiss the dialog
             dialog.dismiss()
         }
         val dialog = builder.create()
         dialog.show()
     }
+
 }
